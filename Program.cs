3ce0284da30a2c8e79e89cbaf9d7c99dd1fb66c5
Martin Stige <martin.stige@axessgroup.com>
2024-04-02 12:53:58 +0200

    Created tool for removing duplicates from PATH variable

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Cleaning PATH variable...");
+
+cleanPathVariable(EnvironmentVariableTarget.User);
+cleanPathVariable(EnvironmentVariableTarget.Machine);
+
+static void cleanPathVariable(EnvironmentVariableTarget target)
+{
+    Console.WriteLine($"Cleaning PATH variable for {target}...");
+
+    try
+    {
+        string pathVariable = Environment.GetEnvironmentVariable("PATH", target) ?? string.Empty;
+
+        if (pathVariable == string.Empty)
+        {
+            Console.WriteLine("PATH variable is empty");
+            return;
+        }
+
+        var pathDirectories = pathVariable.Split(';').ToList();
+
+        Console.WriteLine($"Found {pathDirectories.Count} directories in PATH variable");
+        pathDirectories.ToList().ForEach(Console.WriteLine);
+
+        ISet<string> uniqueDirectories = pathDirectories.ToHashSet();
+
+
+        var numRemoved = pathDirectories.Count - uniqueDirectories.Count;
+        Console.WriteLine($"Removed {numRemoved} duplicate directories");
+
+        string cleanedPath = string.Join(";", uniqueDirectories);
+
+        Environment.SetEnvironmentVariable("PATH", cleanedPath, target);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
